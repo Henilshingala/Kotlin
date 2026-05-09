155591768400109edd9204bdc6c66481dc4365f8
Henil Shingala <henilshingala2462@gmail.com>
2026-05-09 07:39:31 +0530

    2nd program

---
+++ src/2.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var myAge:Int = 17
+    val myName:String = "Henil"
+    println("My Name Is : $myName")
+    println("My Age Is : $myAge")
+    myAge = 18
+    println("After 3 Months My Age Is : $myAge")
+}
